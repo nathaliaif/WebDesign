356a59d5491b019e858cca27075e8ba3210ef14e
Nathalia Irokawa Fernandes <nathalia.iro.fernandes@gmail.com>
2017-02-15 07:54:27 -0200

    Criando Aula 01

---
+++ Array/Array/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Array
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnArray1 = new System.Windows.Forms.Button();
+            this.btnCarta = new System.Windows.Forms.Button();
+            this.btnArray2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnArray3 = new System.Windows.Forms.Button();
+            this.btnArray4 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.btnArray5 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.btnArray6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnArray1
+            // 
+            this.btnArray1.Location = new System.Drawing.Point(32, 30);
+            this.btnArray1.Name = "btnArray1";
+            this.btnArray1.Size = new System.Drawing.Size(75, 23);
+            this.btnArray1.TabIndex = 0;
+            this.btnArray1.Text = "Array 1";
+            this.btnArray1.UseVisualStyleBackColor = true;
+            this.btnArray1.Click += new System.EventHandler(this.btnArray1_Click);
+            // 
+            // btnCarta
+            // 
+            this.btnCarta.Location = new System.Drawing.Point(127, 30);
+            this.btnCarta.Name = "btnCarta";
+            this.btnCarta.Size = new System.Drawing.Size(75, 23);
+            this.btnCarta.TabIndex = 1;
+            this.btnCarta.Text = "Carta";
+            this.btnCarta.UseVisualStyleBackColor = true;
+            this.btnCarta.Click += new System.EventHandler(this.btnCarta_Click);
+            // 
+            // btnArray2
+            // 
+            this.btnArray2.Location = new System.Drawing.Point(227, 30);
+            this.btnArray2.Name = "btnArray2";
+            this.btnArray2.Size = new System.Drawing.Size(75, 23);
+            this.btnArray2.TabIndex = 2;
+            this.btnArray2.Text = "Array 2";
+            this.btnArray2.UseVisualStyleBackColor = true;
+            this.btnArray2.Click += new System.EventHandler(this.btnArray2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(29, 115);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(84, 108);
+            this.listBox1.TabIndex = 3;
+            // 
+            // btnArray3
+            // 
+            this.btnArray3.Location = new System.Drawing.Point(32, 84);
+            this.btnArray3.Name = "btnArray3";
+            this.btnArray3.Size = new System.Drawing.Size(75, 23);
+            this.btnArray3.TabIndex = 4;
+            this.btnArray3.Text = "Array 3";
+            this.btnArray3.UseVisualStyleBackColor = true;
+            this.btnArray3.Click += new System.EventHandler(this.btnArray3_Click);
+            // 
+            // btnArray4
+            // 
+            this.btnArray4.Location = new System.Drawing.Point(127, 84);
+            this.btnArray4.Name = "btnArray4";
+            this.btnArray4.Size = new System.Drawing.Size(75, 23);
+            this.btnArray4.TabIndex = 5;
+            this.btnArray4.Text = "Array 4";
+            this.btnArray4.UseVisualStyleBackColor = true;
+            this.btnArray4.Click += new System.EventHandler(this.btnArray4_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(127, 115);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(75, 108);
+            this.listBox2.TabIndex = 6;
+            // 
+            // btnArray5
+            // 
+            this.btnArray5.Location = new System.Drawing.Point(227, 83);
+            this.btnArray5.Name = "btnArray5";
+            this.btnArray5.Size = new System.Drawing.Size(75, 23);
+            this.btnArray5.TabIndex = 7;
+            this.btnArray5.Text = "Array 5";
+            this.btnArray5.UseVisualStyleBackColor = true;
+            this.btnArray5.Click += new System.EventHandler(this.btnArray5_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(224, 115);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(83, 108);
+            this.listBox3.TabIndex = 8;
+            // 
+            // btnArray6
+            // 
+            this.btnArray6.Location = new System.Drawing.Point(323, 30);
+            this.btnArray6.Name = "btnArray6";
+            this.btnArray6.Size = new System.Drawing.Size(75, 23);
+            this.btnArray6.TabIndex = 9;
+            this.btnArray6.Text = "Array 6";
+            this.btnArray6.UseVisualStyleBackColor = true;
+            this.btnArray6.Click += new System.EventHandler(this.btnArray6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 243);
+            this.Controls.Add(this.btnArray6);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.btnArray5);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.btnArray4);
+            this.Controls.Add(this.btnArray3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnArray2);
+            this.Controls.Add(this.btnCarta);
+            this.Controls.Add(this.btnArray1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnArray1;
+        private System.Windows.Forms.Button btnCarta;
+        private System.Windows.Forms.Button btnArray2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnArray3;
+        private System.Windows.Forms.Button btnArray4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button btnArray5;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button btnArray6;
+    }
+}
+
---
+++ Array/Array/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Array
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnArray1_Click(object sender, EventArgs e)
+        {
+            //Criando e iniciando um simples array
+            //Neste caso o tamanho do Array pode variar 
+            string[] times = { "Corinthians", "Palmeiras", "São Paulo", "Santos" };
+
+            //recuperando o conteúdo de um array
+            MessageBox.Show("Time" + times[0]);
+        }
+
+        private void btnCarta_Click(object sender, EventArgs e)
+        {
+            //Criando os naipes e tipos de cartas
+            string[] face = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Valete", "Dama", "Rei" };
+
+            string[] naipe = { "Ouros", "Paus", "Espadas", "Copas" };
+
+            //A linha abaixo ajuda no entendimento do raciocínio lógico
+            //MessageBox.Show("Carta Sorteada: " + numeros[10] + " de " + naipe[3]);
+
+            Random numero = new Random();
+
+            //Função para gerar números aleatórios dos números e naipes
+            //Criando duas variáveis que vão receber números aleatórios
+            string n = naipe[numero.Next(4)];
+            string f = face[numero.Next(13)];
+
+            //Criando o Array aleatório de Números e Naipes de cartas
+            MessageBox.Show(f + " de " + n);
+        }
+
+        private void btnArray2_Click(object sender, EventArgs e)
+        {
+            //Outra forma de declarar e iniciar um Array 
+            //Neste caso o Array SEMPRE tem um tamanho fixo
+
+            //"Criando um objeto Array do tipo inteiro de tamanho 5"
+
+            int[] numeros = new int[5];
+            numeros[0] = 1;
+            numeros[1] = 5;
+            numeros[2] = 18;
+            numeros[3] = 45;
+            numeros[4] = 90;
+
+            MessageBox.Show("Número: " + numeros[2]);
+        }
+
+        private void btnArray3_Click(object sender, EventArgs e)
+        {
+            //Preenchendo o Array com o laço for
+            int[] numeros = new int[5];
+
+            //A estrutura abaixo cria as variáveis que irão preencher as 5 "casinhas" do Array
+
+            //++ equivale a i=i+1
+            for (int i = 0; i < 5; i++)
+            {
+                numeros[i] = i;
+                //listBox1.Items.Add(i);
+                listBox1.Items.Add("numeros [" + i + "] = " + i);
+            }
+        }
+
+        private void btnArray4_Click(object sender, EventArgs e)
+        {
+            //Outra forma de iniciar um Array
+            //Uso do laço foi para exibir o conteúdo do Array
+            //Uso do .Length
+
+            int[] pares = new int[] { 2, 4, 6, 8 };
+
+            // .Length -- identifica o tamanho total do Array
+            for (int i = 0; i < pares.Length; i++)
+            {
+                listBox2.Items.Add(pares[i]);
+            }
+        }
+
+        private void btnArray5_Click(object sender, EventArgs e)
+        {
+            //Uso do foreach (laço for simplificado)
+            string[] semana = new string[7];
+            semana[0] = "Segunda";
+            semana[1] = "Terça";
+            semana[2] = "Quarta";
+            semana[3] = "Quinta";
+            semana[4] = "Sexta";
+            semana[5] = "Sábado";
+            semana[6] = "Domingo";
+            
+
+            //foreach (tipo de varávelnome (qualquer nome) in nome_Array)
+            foreach (string dia in semana)
+            {
+                listBox3.Items.Add(dia);
+            }
+
+        }
+
+        private void btnArray6_Click(object sender, EventArgs e)
+        {
+            // Array multidimensional "MATRIZ"
+            // [,] 2 dimensões [linha,coluna]
+            //[,,] 3 dimensaões 
+            // [,,,] 4 dimensões
+            // ....
+            // a linha abaixo cria e inicaliza um array de 2 dimensões com 2 linhas e 4 colunas [2,4]
+            double[,] notas = new double[2, 4] { { 8,7,9,3 }, {4, 5, 8, 6 } };
+            // exemplo da tabela de notas do aluno
+            MessageBox.Show("Nota: " + notas[0, 2]);
+
+        }
+    }
+}
